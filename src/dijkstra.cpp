9294be53389d56aee92c20ef2b48b15d8c10e60f
Diego Hommerding Amorim <diego@int6tech.com.br>
2025-03-13 22:02:54 -0300

    fully implemented min k-ary heap

---
+++ src/dijkstra.cpp
@@ -0,0 +1,45 @@
+#include <boost/graph/adjacency_list.hpp>
+#include <vector>
+#include <limits>
+#include <iostream>
+using namespace boost;
+
+#include "heap.cpp"
+
+typedef adjacency_list<vecS, vecS, directedS> Graph;
+
+int dijkstra(Graph graph, int source_vertex, int destiny_vertex){
+    int num_verts = num_vertices(graph);
+
+    // Initialize distance map
+    std::vector<bool> visited(num_verts, false);
+    visited[source_vertex] = true;
+
+    // Initialize priority queue
+    KHeap priority_queue(num_verts);
+    priority_queue.insert(source_vertex, 0);
+
+    while(priority_queue)
+    
+
+
+
+    return 0;
+}
+
+
+// 1 ds := 0; dv := ∞, ∀v ∈ V \ {s}
+// 2 visited(v) := false, ∀v ∈ V
+// 3 Q := ∅
+// 4 insert(Q, (s, 0))
+// 5 while Q̸ = ∅ do
+// 6 v := deletemin(Q)
+// 7 visited(v) := true
+// 8 for u ∈ N+(v) do
+// 9 if not visited(u) then
+// 10 if du = ∞ then
+// 11 du := dv + dvu
+// 12 insert(Q, (u, du))
+// 13 else if dv + dvu < du
+// 3Na hipótese razoável que m ≥ n.
+// 9
---
+++ src/heap.cpp
@@ -0,0 +1,89 @@
+#include <array>
+#include <vector>
+#include <limits>
+#include <unordered_map>
+
+struct Node {
+    int vertex, dist;
+    bool operator<(const Node& other) const {
+        return dist < other.dist;  
+    }
+};
+
+class KHeap {
+    public:
+        static const int k = 3;
+
+        KHeap(int total_vertices) : pos(total_vertices, -1){}
+
+        Node deletemin(){
+            Node min = heap.front();
+            pos[min.vertex] = -1;
+            swap_nodes(0, heap.size()-1);
+            heap.pop_back();
+            sift_down(0);
+            return min;
+        }
+
+        void update(int vertex, int dist){
+            int vertex_index = pos[vertex];
+            heap[vertex_index] = Node{vertex,dist};
+            sift_up(vertex_index);
+            sift_down(vertex_index);
+        }
+
+        void insert(int vertex, int dist){
+            heap.push_back(Node{vertex,dist});
+            pos[vertex] = heap.size()-1;
+            sift_up(heap.size()-1);
+        }
+
+        int get_size(){
+            return heap.size();
+        }
+
+    private:
+        std::vector<Node> heap;
+        std::vector<int> pos;
+
+        void swap_nodes(int index, int index2){
+            int v1 = heap[index].vertex;
+            int v2 = heap[index2].vertex;
+
+            std::swap(heap[index], heap[index2]);   // Swap head distances
+            std::swap(pos[v1], pos[v2]);            // Swap hash indexes
+        }
+        
+        void sift_up(int index) {
+            while(index > 0){
+                int parent = (index - 1) / k;
+
+                if(heap[index] < heap[parent]){
+                    swap_nodes(index,parent);
+                    index = parent;
+                }else{
+                    break;
+                }
+            }
+        }
+
+        void sift_down(int index) {
+            while(true){
+                int min = index;
+
+                for (int i = 1; i <= k; i++) {
+                    int child = (k * index) + i;
+                    if (child < heap.size() && heap[child] < heap[min]) {
+                        min = child;
+                    }
+                }
+
+                if(min != index){
+                    swap_nodes(index,min);
+                    index = min;
+                }else{
+                    break;
+                }
+            }
+        }
+};
